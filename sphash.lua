19f2fa50c74265b37df2837a1ae9b426a8714c2f
Borg <borg@uu3.net>
2012-04-14 15:17:38 +0200

    Cleanup in sphash.lua

--- sphash.lua
+++ sphash.lua
@@ -5,8 +5,8 @@ require "class"
 local function spkeys(grid,x1,y1,x2,y2)
   local sx=math.floor(x1/grid)
   local sy=math.floor(y1/grid)
-  local mx=math.floor(x2/grid)
-  local my=math.floor(y2/grid)
+  local mx=x2 and math.ceil(x2/grid) or sx
+  local my=y2 and math.ceil(y2/grid) or sy
   local x,y
   if mx<sx then
     x=mx
@@ -26,7 +26,7 @@ local function spkeys(grid,x1,y1,x2,y2)
     if y>my then
       return nil
     end
-    local k=x..","..y
+    local k=string.format("%d,%d",x,y)
     x=x+1
     if x>mx then
       x=sx
@@ -61,40 +61,14 @@ function sphash(grid)
     end
   end
   function object:get(x1,y1,x2,y2)
-    local grid=self.grid
-    local sx=math.floor(x1/grid)
-    local sy=math.floor(y1/grid)
-    local mx=x2 and math.floor(x2/grid) or sx
-    local my=y2 and math.floor(y2/grid) or sy
-    local x,y
-    if mx<sx then
-      x=mx
-      mx=sx
-      sx=x
-    else
-      x=sx
-    end
-    if my<sy then
-      y=my
-      my=sy
-      sy=y
-    else
-      y=sy
-    end
     local ret={}
-    local k,t,v
-    while y<=my do
-      k=x..","..y
+    local t,v
+    for k in spkeys(self.grid,x1,y1,x2,y2) do
       t=self.hash[k]
       if t then
         for _,v in pairs(t) do
           ret[v]=v
         end
-      end
-      x=x+1
-      if x>mx then
-        x=sx
-        y=y+1
       end
     end
     return ret
