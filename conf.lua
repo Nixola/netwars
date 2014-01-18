c6f39adf877803ded43de419ac692d174bf2fad5
Borg <borg@uu3.net>
2014-01-18 20:51:22 +0100

    Fixed conf.lua

--- conf.lua
+++ conf.lua
@@ -5,8 +5,8 @@ function love.conf(t)
   t.author="Borg <borg@uu3.net>"
   t.identity="netwars"
   t.console=false
-  t.screen.width=800
-  t.screen.height=600
+  t.window.width=800
+  t.window.height=600
   t.modules.joystick=false
   t.modules.physics=false
   t.modules.audio=false
